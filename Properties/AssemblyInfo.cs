c87b47f5f53103a7758da4657bf63b9a250d99f2
Daniel Kandalaft <kandddany1985@gmail.com>
2024-11-09 12:09:33 +0200

    Docker for Intermediate

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("066dfc63-2d19-4dd0-a938-c2d55f945aec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tests/SanityTestGrid.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Drawing;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace CSProject.Tests
+{
+    [TestFixture]
+    class SanityTestGrid
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            string nodeURL = "http://localhost:4444";
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("--no-sandbox");
+            driver = new RemoteWebDriver(new Uri(nodeURL), options);
+            driver.Manage().Window.Size = new Size(1920, 1080);
+            driver.Manage().Window.Position = new Point(620, 0);
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+            Login("standard_user", "secret_sauce");
+        }
+
+        [Test]
+        public void Test01_CountItems()
+        {
+            SelectAllItems();
+            VerifyNumberOfItemsCart("6");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        public void Login(string username, string password)
+        {
+            driver.FindElement(By.Id("user-name")).SendKeys(username);
+            driver.FindElement(By.Id("password")).SendKeys(password);
+            driver.FindElement(By.Id("login-button")).Click();
+        }
+
+        public void SelectAllItems()
+        {
+            var items = driver.FindElements(By.ClassName("inventory_item_name"));
+            int numItems = items.Count;
+            for (int i = 0; i < numItems; i++)
+            {
+                items[i].Click();
+                driver.FindElement(By.CssSelector("button[class='btn btn_primary btn_small btn_inventory")).Click();
+                driver.FindElement(By.Id("back-to-products")).Click();
+                items = driver.FindElements(By.ClassName("inventory_item_name"));
+            }
+        }
+
+        public void VerifyNumberOfItemsCart(String expected)
+        {
+            Assert.That(driver.FindElement(By.ClassName("shopping_cart_badge")).Text.Equals(expected));
+        }
+    }
+}
